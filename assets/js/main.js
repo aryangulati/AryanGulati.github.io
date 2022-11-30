70b1270dcab55784ffa483337600c2175a2d3dbe
aryangulati <42711978+aryangulati@users.noreply.github.com>
2022-11-30 23:07:13 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,23 +35,23 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 200}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.button',{delay: 0}); 
+sr.reveal('.home__img',{delay: 100}); 
+sr.reveal('.home__social-icon',{ interval: 100}); 
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{delay: 200}); 
-sr.reveal('.about__subtitle',{delay: 200}); 
-sr.reveal('.about__text',{delay: 200}); 
+sr.reveal('.about__img',{delay: 0}); 
+sr.reveal('.about__subtitle',{delay: 0}); 
+sr.reveal('.about__text',{delay: 0}); 
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{delay: 200}); 
-sr.reveal('.skills__text',{delay: 200}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 200});
+sr.reveal('.skills__subtitle',{delay: 0}); 
+sr.reveal('.skills__text',{delay: 0}); 
+sr.reveal('.skills__data',{interval: 0}); 
+sr.reveal('.skills__img',{delay: 0});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 100}); 
+sr.reveal('.work__img',{interval: 0}); 
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 100});
